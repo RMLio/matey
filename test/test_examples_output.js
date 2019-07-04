94946977fed39efcd13f3e37453c13517f73cc6b
liouka antchougov <liouka.antchougov@ugent.be>
2019-07-04 12:44:22 +0200

    added unfinished test for ld output

--- test/test_examples_output.js
+++ test/test_examples_output.js
@@ -1,5 +1,73 @@
-mocha.setup('bdd');
+/**
+   Asynchronously reads content of file and performs given callback when done
+ * @param  {String} path to file that must be read
+ * @param  {Function} callback that will be performed when file is read
+ */
+function readFileContent(path, callback) {
+    let rawFile = new XMLHttpRequest();
+    rawFile.open("GET", path, false);
+    rawFile.onreadystatechange = function () {
+        if(rawFile.readyState === 4) {
+            if(rawFile.status === 200 || rawFile.status === 0) {
+                let text = rawFile.responseText;
+                callback(text);
+            }
+        }
+    };
+    rawFile.send(null);
+}
 
-describe('Examples Output', function() {
-   describe('')
+/**
+ * Returns the text inside the ace editor with the given id.
+ * @param  {String} id of the editor element
+ */
+function getAceEditorText(id) {
+    let lines = [];
+    // iterate over divs that contain content lines of ace editor and add them to result
+    let $line_divs = $("#" + id).find(".ace_scroller").find(".ace_content").find(".ace_text-layer").children();
+    $line_divs.each(function(index, value) {
+        let line = $(this).text();
+        lines.push(line);
+    });
+    return lines.join("\n");
+}
+
+/**
+ * Checks whether clicking the "Generate LD" button generates the correct output in the Turtle/TriG editor
+ * @param  {String} filename: path to file where correct output is located
+ * @param {Boolean} isLD: if true, the LD output is checked, otherwise the RML output will be checked
+ * @param {Function} done: callback that indicates test is finished
+ */
+function testOutput(filename, isLD, done) {
+    let buttonID = isLD ? "#ld-btn" : "#btn";
+        // click the button & wait 2 seconds for output to generate
+    $(buttonID).trigger("click");
+    setTimeout(function() {
+        let generated_output = getAceEditorText("output");
+        // read the correct output from the file and check if it fits the generated output
+        let filepath = 'correct_example_outputs/' + example + "_" + outputType;
+        readFileContent(filepath, function(expected_output) {
+            chai.assert.equal(generated_output, expected_output);
+            done();
+        });
+    }, 2000);
+}
+
+
+describe('Examples Output Test', function() {
+
+    this.timeout(15000);
+
+    describe('People (JSON)', function() {
+
+        it('Turtle/TriG', function(done) {
+            testOutput('people_turtle.txt', true, done);
+        });
+
+        it('RML', function() {
+            testOutput('people_rml.txt', true, done);
+        });
+    });
+
+
 });
