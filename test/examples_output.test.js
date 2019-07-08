83c0865cd2c36418823061651a3f2c9e317dbbad
liouka antchougov <liouka.antchougov@ugent.be>
2019-07-08 10:43:43 +0200

    example output tests now working and all pass

--- test/examples_output.test.js
+++ test/examples_output.test.js
@@ -1,8 +1,18 @@
+'use strict';
+
 // import necessary modules
 const $ = require('jquery');
+
 const matey = require('../');
+
 const fs = require('fs');
-const examples = require('../matey/global').examples;
+
+let examples = require('../matey/global').examples;
+
+const N3 = require('n3');
+const parser = new N3.Parser();
+const isomorphic = require('rdf-isomorphic').isomorphic;
+
 require('jsdom-worker');
 
 // set up document body
@@ -17,7 +27,7 @@ describe('Examples Output Test', function() {
 
     describe('People (JSON)', function() {
 
-        // load the People (JSON) example
+        // load the 'People (JSON)' example
         matey.loadExample(examples[0]);
 
         it('Turtle/TriG has correct output', function(done) {
@@ -27,12 +37,11 @@ describe('Examples Output Test', function() {
         it('RML has correct output', function(done) {
             testOutput('people_rml.txt', false, done);
         });
-
     });
 
     describe('Advanced', function() {
 
-        // load the Advanced example
+        // load the 'Advanced' example
         matey.loadExample(examples[1]);
 
         it('Turtle/TriG has correct output', function(done) {
@@ -42,12 +51,12 @@ describe('Examples Output Test', function() {
         it('RML has correct output', function(done) {
             testOutput('advanced_rml.txt', false, done);
         });
-
     });
+
 
     describe('Facebook', function() {
 
-        // load the Facebook example
+        // load the 'Facebook' example
         matey.loadExample(examples[2]);
 
         it('Turtle/TriG has correct output', function(done) {
@@ -57,9 +66,7 @@ describe('Examples Output Test', function() {
         it('RML has correct output', function(done) {
             testOutput('facebook_rml.txt', false, done);
         });
-
     });
-
 
 });
 
@@ -68,7 +75,7 @@ describe('Examples Output Test', function() {
  * in the corresponding editor
  * @param  {String} filename: path to file where correct output is located
  * @param {Boolean} checkLD: if true, the LD output is checked, otherwise the RML output will be checked
- * @param {Function} done: callback that, indicates test is finished when called
+ * @param {Function} done: callback that indicates test is finished when called
  */
 function testOutput(filename, checkLD, done) {
     let buttonID = checkLD ? "#ld-btn" : "#btn";
@@ -76,13 +83,22 @@ function testOutput(filename, checkLD, done) {
     // click the button & wait 2 seconds for output to generate
     $(buttonID).trigger("click");
 
-    // retrieve generated output from editor
-    let generated_output = checkLD ? matey.getLD() : matey.getRML();
+    setTimeout(function() {
+        // retrieve generated output from editor
+        let generated_output = checkLD ? matey.getLD() : matey.getRML();
 
-    // read the correct output from the file and check if it fits the generated output
-    let path = __dirname + "/correct_example_outputs/" + filename;
-    let expected_output = fs.readFileSync(path, 'utf8');
-    expect(generated_output).toBe(expected_output);
+        // read the correct output from the file
+        let path = __dirname + "/correct_example_outputs/" + filename;
+        let expected_output = fs.readFileSync(path, 'utf8');
 
-    done();
+        // convert the outputs to RDF quads
+        let generated_quads = parser.parse(generated_output);
+        let expected_quads = parser.parse(expected_output);
+
+        // check if the expected quads are isomorphic to the generated quads
+        expect(isomorphic(generated_quads, expected_quads)).toBe(true);
+
+        // say that test is done
+        done();
+    }, 2000);
 }
