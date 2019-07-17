ed27aa6adde3061975d60087e7564c4aea6fd5a6
liouka antchougov <liouka.antchougov@ugent.be>
2019-07-17 14:33:32 +0200

    added configuration of RMLMapper endpoint to examples

--- examples/with_require/init.js
+++ examples/with_require/init.js
@@ -3,4 +3,7 @@ let matey = new Matey();
 
 document.body.innerHTML = '<div id="test"></div>';
 
-matey.init("test");
+let config = {
+  rmlMapperUrl: "http://localhost:4000/execute" // make sure an endpoint with this URL is active!
+};
+matey.init("test", config);
