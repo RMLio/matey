312a2623b5648fa6aa0fb46d1a2cdb3695b728f8
renevds <rene.v.d.s.9@gmail.com>
2021-08-09 17:09:27 +0200

    Fix rmlMapperUrl in init.js

--- examples/with_bundler/init.js
+++ examples/with_bundler/init.js
@@ -4,7 +4,7 @@ const matey = new Matey();
 document.body.innerHTML = '<div id="test"></div>';
 
 const config = {
-  rmlMapperUrl: "http://172.23.43.193:4000/execute" // make sure an endpoint with this URL is active!
+  rmlMapperUrl: "http://localhost:4000/execute" // make sure an endpoint with this URL is active!
 };
 
 //for debug
