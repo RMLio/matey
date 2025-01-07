e913ad8e0a63e2eb78b6e1e4b4471649a96defae
Dylan Van Assche <dylan.vanassche@ugent.be>
2025-01-07 13:14:49 +0100

    assets: html: matey: bring back RML output

--- test/generate_ld_rml_test/generate-ld-rml.test.js
+++ test/generate_ld_rml_test/generate-ld-rml.test.js
@@ -20,27 +20,27 @@ global.structuredClone = (val) => JSON.parse(JSON.stringify(val))
 // runMappingRemote() tests
 describe('runMappingRemote()', function() {
 
-    it('should generate correct LD output for "People (JSON)" example', async function() {
+    it('should generate correct LD output for "Simple JSON source" example', async function() {
         matey.loadExample(examples[0]);
         await testOutput('people_ld.ttl', true);
     });
 
-    it('should generate correct LD output for "Advanced" example', async function() {
+    it('should generate correct LD output for "Combination of a JSON and a CSV source" example', async function() {
         matey.loadExample(examples[1]);
         await testOutput('advanced_ld.ttl', true);
     });
 
-    it('should generate correct LD output for "Facebook" example', async function() {
+    it('should generate correct LD output for "Facebook JSON data" example', async function() {
         matey.loadExample(examples[2]);
         await testOutput('facebook_ld.ttl', true);
     });
 
-    it('should generate correct LD output for "Target" example', async function() {
+    it('should generate correct LD output for "YARRRML Targets for specifying output 1" example', async function() {
         matey.loadExample(examples[3]);
         await testOutput('target_ld_0.ttl', true, 0);
     });
 
-    it('should generate correct LD output for "Target" example', async function() {
+    it('should generate correct LD output for "YARRRML Targets for specifying output 2" example', async function() {
         matey.loadExample(examples[3]);
         await testOutput('target_ld_1.ttl', true, 1);
     });
@@ -49,32 +49,32 @@ describe('runMappingRemote()', function() {
 // toRML() tests
 describe('toRML()', function() {
 
-    it('should generate correct RML output for "People (JSON)" example', async function() {
+    it('should generate correct RML output for "Simple JSON source" example', async function() {
         matey.loadExample(examples[0]);
         await testOutput('people_rml.ttl', false);
     });
 
-    it('should generate correct RML output for "Advanced" example', async function() {
+    it('should generate correct RML output for "Combination of a JSON and a CSV source" example', async function() {
         matey.loadExample(examples[1]);
         await testOutput('advanced_rml.ttl', false);
     });
 
-    it('should generate correct RML output for "Facebook" example', async function() {
+    it('should generate correct RML output for "Facebook JSON" example', async function() {
         matey.loadExample(examples[2]);
         await testOutput('facebook_rml.ttl', false);
     });
 
-    it('should generate correct RML output for "Target" example', async function() {
+    it('should generate correct RML output for "YARRRML Targets for specifying output" example', async function() {
         matey.loadExample(examples[3]);
         await testOutput('target_rml.ttl', false);
     });
 
-    it('should generate correct RML output for "IncRML + LDES 1" example', async function() {
+    it('should generate correct RML output for "Basic LDES of sensor data" example', async function() {
         matey.loadExample(examples[5]);
         await testOutput('basic_ldes_1_rml.ttl', false);
     });
 
-    it('should generate correct RML output for "IncRML + LDES 2" example', async function() {
+    it('should generate correct RML output for "Updated LDES of sensor data" example', async function() {
         matey.loadExample(examples[6]);
         await testOutput('basic_ldes_2_rml.ttl', false);
     });
@@ -100,7 +100,7 @@ async function testOutput(filename, checkLD, index=0) {
     }
 
     // retrieve generated output from editor
-    const generatedOutput = checkLD ? matey.getLD()[index] : matey.getRML();
+    const generatedOutput = checkLD ? matey.getLD()[index+1] : matey.getLD()[0]
 
     // read the correct output from the file
     const path = __dirname + "/correct_example_outputs/" + filename;
